cf97a76290b25a681d7782dd4db3c438066d8fe3
yjhoon2 <whdgnsl06@naver.com>
2021-06-13 10:32:59 +0900

    6/13 update

---
+++ Programmers/Level2/소수_찾기.py
@@ -0,0 +1,11 @@
+
+# 에라토스테네스의 체
+def solution(n):
+    ch = [0] * (n+1)
+    cnt = 0
+    for i in range(2, n + 1):
+        if ch[i] == 0:
+            cnt += 1
+            for j in range(i, n+1, i):
+                ch[j] = 1
+    return cnt

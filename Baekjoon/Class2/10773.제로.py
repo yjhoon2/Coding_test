dfc275fdd95cb0a3eafd9bb93f00a48ed10ffe31
yjhoon2 <whdgnsl06@naver.com>
2021-06-16 12:46:34 +0900

    6/16 update

---
+++ Baekjoon/Class2/10773.제로.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin = open("input.txt", "rt")
+input = sys.stdin.readline
+
+
+n = int(input())
+stack = []
+for _ in range(n):
+    a = int(input())
+    if a == 0:
+        stack.pop()
+    else:
+        stack.append(a)
+print(sum(stack))
+
+
+# 이게 시간 40배는 단축해줌
+from sys import stdin
+
+input()
+stack = []
+for x in map(int, stdin):
+    print(x)
+    if x:
+        stack.append(x)
+    else:
+        stack.pop()
+
+print(sum(stack))

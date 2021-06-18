96ca96a76f7c9c1f5f6a7a26ccd79343cd627afe
yjhoon2 <whdgnsl06@naver.com>
2021-06-18 13:15:08 +0900

    6/18 update

--- Baekjoon/Class2/10989.수_정렬하기3.py
+++ Baekjoon/Class2/10989.수_정렬하기3.py
@@ -1,16 +1,7 @@
 import sys
 sys.stdin = open("input.txt", "rt")
 input = sys.stdin.readline
-'''
-f = open(0) 
-f.readline()
-li=[0]*10001
-for line in f:
-    li[int(line)] += 1
 
-for i in range(1, 10001):
-    print(f'{i}\n' * li[i], end='')
-'''
 import sys
 
 n = int(sys.stdin.readline())
@@ -23,6 +14,9 @@ for i in range(10001):
     if res[i] != 0:
         print(f'{i}\n' * res[i], end = '')
 
+
+
+# 원래 풀이인데 메모리 초과로 에러나서 안됨
 '''
 import sys
 
@@ -32,4 +26,16 @@ for _ in range(n):
     res.append(int(sys.stdin.readline()))
 res.sort()
 print('\n'.join(map(str, res)))
+'''
+
+# 최단시간 기록한 다른사람 풀이
+'''
+f = open(0) 
+f.readline()
+li=[0]*10001
+for line in f:
+    li[int(line)] += 1
+
+for i in range(1, 10001):
+    print(f'{i}\n' * li[i], end='')
 '''

511b15d71c0b89dbe3189e95427b9c1ca77cab10
yjhoon2 <whdgnsl06@naver.com>
2021-08-05 14:03:56 +0900

    8/5 update

--- Programmers/Level3/단어_변환.py
+++ Programmers/Level3/단어_변환.py
@@ -1,7 +1,29 @@
-def DFS(begin, target, words, )
+# 아니 개어렵네 ㅁㅊ...
+
+def DFS(begin, target, words, visit):
+    answer = 0
+    stack = [begin]
+    while stack:
+        w = stack.pop()
+        if w == target:
+            return answer
+        
+        for i in range(len(words)):
+            diff = 0
+            if visit[i] == 0:
+                for a, b in zip(w, words[i]):
+                    if a != b:
+                        diff += 1
+                if diff == 1:
+                    stack.append(words[i])
+                    visit[i] = 1
+        answer += 1
+    return answer
+        
 
 def solution(begin, target, words):
+    visit = [0] * len(words)
     if target not in words:
         return 0
     
-    return answer
+    return DFS(begin, target, words, visit)
